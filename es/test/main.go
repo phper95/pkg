4c4dcb182726ed672310003bfc4cb0da0ae0f9a7
phper95 <1366295050@qq.com>
2022-06-14 00:48:06 +0800

    es test

--- es/test/main.go
+++ es/test/main.go
@@ -74,16 +74,16 @@ func main() {
 	//	Favorites:      1939,
 	//}
 	//id := strconv.FormatInt(doc.Id, 10)
-	//err = esClient.Create(ctx, IndexName, id, "", doc)
+	//err := esClient.Create(ctx, IndexName, id, "", doc)
 	//if err != nil {
 	//	es.EStdLogger.Print(err)
 	//}
-	//err = esClient.Update(ctx, IndexName, id, "", map[string]interface{}{"name": "name2"})
+	//err := esClient.Update(ctx, IndexName, id, "", map[string]interface{}{"name": "name2"})
 	//if err != nil {
 	//	es.EStdLogger.Print(err)
 	//}
 	//doc.Name = "name4"
-	//err = esClient.UpsertWithVersion(ctx, IndexName, id, "", doc, 4)
+	//err := esClient.UpsertWithVersion(ctx, IndexName, id, "", doc, 3)
 	//if err != nil {
 	//	es.EStdLogger.Print(err)
 	//}
@@ -96,25 +96,38 @@ func main() {
 	//if err != nil {
 	//	es.EStdLogger.Print(err)
 	//}
+	/*
+			for i := 0; i < 10; i++ {
+				docID := strconv.Itoa(i)
+				//update := map[string]interface{}{"name": "xxx"}
+				doc := Goods{
+					Id:             int64(i),
+					Name:           "name" + docID,
+					Price:          float64(i),
+					Year:           2022,
+					LastMonthSales: i,
+					Favorites:      i,
+				}
+				//esClient.BulkUpsert(IndexName, docID, docID, update, doc)
+				//esClient.BulkCreate(IndexName, docID, docID, doc)
+				esClient.BulkReplace(IndexName, docID, docID, doc)
+			}
+
+		//因为是异步处理，这里需要等待本地channel提交
+		time.Sleep(3 * time.Second)
+	*/
+
 	/**
-	for i := 0; i < 10; i++ {
-		docID := strconv.Itoa(i)
-		//update := map[string]interface{}{"name": "xxx"}
-		doc := Goods{
-			Id:             int64(i),
-			Name:           "name" + docID,
-			Price:          float64(i),
-			Year:           2022,
-			LastMonthSales: i,
-			Favorites:      i,
-		}
-		//esClient.BulkUpsert(IndexName, docID, docID, update, doc)
-		//esClient.BulkCreate(IndexName, docID, docID, doc)
-		esClient.BulkReplace(IndexName, docID, docID, doc)
+	//UpdateByQuery
+
+	updateScript := `ctx._source.name=params.name;ctx._source.favorites=params.favorites`
+	//注意：map中的键必须和updateScript中params.后面的字段名一一对应，大小写及命名方式要完全一致
+	updateParams := map[string]interface{}{"name": "name123", "favorites": 123}
+	_, err := esClient.UpdateByQuery(ctx, IndexName, []string{"1", "2", "3"}, elastic.NewRangeQuery("id").Lte(3), updateScript, updateParams)
+	if err != nil {
+		es.EStdLogger.Print(err)
 	}
 
-	//因为是异步处理，这里需要等待本地channel提交
-	time.Sleep(3 * time.Second)
 	*/
 
 	/*
@@ -143,6 +156,5 @@ func main() {
 		}
 
 		es.EStdLogger.Printf("%+v", goods)
-	
 	*/
 }
