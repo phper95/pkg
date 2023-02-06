053319b128cb59f917088ed9377032fe3ac8a4a3
phper95 <1366295050@qq.com>
2023-02-07 07:28:25 +0800

    update

--- es/doc_operation.go
+++ es/doc_operation.go
@@ -37,7 +37,7 @@ type BulkUpdateDoc struct {
 	update map[string]interface{}
 }
 
-//新建文档
+// 新建文档
 func (c *Client) Create(ctx context.Context, indexName, id, routing string, doc interface{}) error {
 	//注意 sdk这里第一个index获取的是*IndexService，即索引服务，第二个index是指定需要写入的索引名
 	indexService := c.Client.Index().Index(indexName).OpType("create")
@@ -55,7 +55,7 @@ func (c *Client) Create(ctx context.Context, indexName, id, routing string, doc 
 	return err
 }
 
-//批量的方式新建文档，后台提交
+// 批量的方式新建文档，后台提交
 func (c *Client) BulkCreate(indexName, id, routing string, doc interface{}) {
 	bulkCreateRequest := elastic.NewBulkCreateRequest().Index(indexName).Doc(doc)
 	if len(id) > 0 {
@@ -67,7 +67,7 @@ func (c *Client) BulkCreate(indexName, id, routing string, doc interface{}) {
 	c.BulkProcessor.Add(bulkCreateRequest)
 }
 
-//批量的方式新建一批文档,实时提交
+// 批量的方式新建一批文档,实时提交
 func (c *Client) BulkCreateDocs(ctx context.Context, indexName string, docs []*BulkCreateDoc) (*elastic.BulkResponse, error) {
 	bulkService := c.Client.Bulk().ErrorTrace(true)
 	for _, doc := range docs {
@@ -110,13 +110,13 @@ func (c *Client) BulkReplaceDocs(ctx context.Context, indexName string, docs []*
 	return bulkService.Do(ctx)
 }
 
-//业务数据修改时生成版本号，在更新ES文档数据时，根据这个版本号来避免重复和乱序处理，实现操作的幂等性
-//PUT twitter/_doc/1?version=2&version_type=external
-//{
-//"message" : "elasticsearch now has versioning support, double cool!"
-//}
-//1.版本管理是完全实时的，不受搜索操作的近实时方面的影响
-//2.如果当前提供的版本号比实际的版本号大（或者实际文档不存在）就会成功写入，并设置为当前的版本号，否则返回失败（409状态码）
+// 业务数据修改时生成版本号，在更新ES文档数据时，根据这个版本号来避免重复和乱序处理，实现操作的幂等性
+// PUT twitter/_doc/1?version=2&version_type=external
+// {
+// "message" : "elasticsearch now has versioning support, double cool!"
+// }
+// 1.版本管理是完全实时的，不受搜索操作的近实时方面的影响
+// 2.如果当前提供的版本号比实际的版本号大（或者实际文档不存在）就会成功写入，并设置为当前的版本号，否则返回失败（409状态码）
 func (c *Client) BulkCreateWithVersion(ctx context.Context, indexName, id, routing string, version int64, doc interface{}) {
 	bulkCreateRequest := elastic.NewBulkIndexRequest().Index(indexName).Doc(doc).VersionType(DefaultVersionType).Version(version)
 	if len(id) > 0 {
@@ -155,7 +155,7 @@ func (c *Client) DeleteWithVersion(ctx context.Context, indexName, id, routing s
 	return err
 }
 
-//删除过程中如果命中的文档被修改会导致删除出现版本冲突，为了彻底删除文档，需要指定ProceedOnVersionConflict
+// 删除过程中如果命中的文档被修改会导致删除出现版本冲突，为了彻底删除文档，需要指定ProceedOnVersionConflict
 func (c *Client) DeleteByQuery(ctx context.Context, indexName, id, routing string, query elastic.Query) error {
 	deleteByQueryService := c.Client.DeleteByQuery(indexName).Query(query).ProceedOnVersionConflict().Refresh(DefaultRefresh)
 	if len(routing) > 0 {
@@ -168,7 +168,7 @@ func (c *Client) DeleteByQuery(ctx context.Context, indexName, id, routing strin
 func (c *Client) BulkDelete(indexName, id, routing string) {
 	bulkDeleteRequest := elastic.NewBulkDeleteRequest().Index(indexName).Id(id)
 	if len(routing) > 0 {
-		bulkDeleteRequest.Id(routing)
+		bulkDeleteRequest.Routing(routing)
 	}
 	c.BulkProcessor.Add(bulkDeleteRequest)
 }
@@ -176,7 +176,7 @@ func (c *Client) BulkDelete(indexName, id, routing string) {
 func (c *Client) BulkDeleteWithVersion(indexName, id, routing string, version int64) {
 	bulkDeleteRequest := elastic.NewBulkDeleteRequest().Index(indexName).Id(id).VersionType(DefaultVersionType).Version(version)
 	if len(routing) > 0 {
-		bulkDeleteRequest.Id(routing)
+		bulkDeleteRequest.Routing(routing)
 	}
 	c.BulkProcessor.Add(bulkDeleteRequest)
 }
@@ -227,7 +227,7 @@ func (c *Client) BulkUpdateDocs(ctx context.Context, index string, updates []*Bu
 	return bulkService.Do(ctx)
 }
 
-//文档结构必须完整
+// 文档结构必须完整
 func (c *Client) UpsertWithVersion(ctx context.Context, indexName, id, routing string, doc interface{}, version int64) error {
 	indexService := c.Client.Index().OpType("index").Index(indexName).Id(id).Refresh(DefaultRefresh).Version(version).VersionType(DefaultVersionType)
 	if len(routing) > 0 {
@@ -237,7 +237,7 @@ func (c *Client) UpsertWithVersion(ctx context.Context, indexName, id, routing s
 	return err
 }
 
-//不存在则插入
+// 不存在则插入
 func (c *Client) Upsert(ctx context.Context, indexName, id, routing string, update map[string]interface{}, doc interface{}) error {
 	updateService := c.Client.Update().Index(indexName).Id(id).Refresh(DefaultRefresh).DocAsUpsert(true)
 	if len(routing) > 0 {
@@ -247,7 +247,7 @@ func (c *Client) Upsert(ctx context.Context, indexName, id, routing string, upda
 	return err
 }
 
-//异步upsert
+// 异步upsert
 func (c *Client) BulkUpsert(indexName, id, routing string, update map[string]interface{}, doc interface{}) {
 	bulkUpdateRequest := elastic.NewBulkUpdateRequest().Index(indexName).Doc(update).Id(id).Upsert(doc).DocAsUpsert(true)
 	if len(routing) > 0 {
