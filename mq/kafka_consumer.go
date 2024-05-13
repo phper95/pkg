cee9364e5478d292b4dc9f23013bae625f0c746f
少林码僧 <1366295050@qq.com>
2024-05-13 21:47:40 +0800

    Update kafka_consumer.go

--- mq/kafka_consumer.go
+++ mq/kafka_consumer.go
@@ -15,6 +15,7 @@ import (
 	"github.com/eapache/go-resiliency/breaker"
 )
 
+
 const (
 	//KafkaConsumerConnected 消费者已连接
 	KafkaConsumerConnected string = "connected"
@@ -23,23 +24,21 @@ const (
 )
 
 type Consumer struct {
-	hosts    []string
-	topics   []string
-	config   *cluster.Config
-	consumer *cluster.Consumer
-	status   string
-	groupID  string
-
+	hosts      []string
+	topics     []string
+	config     *cluster.Config
+	consumer   *cluster.Consumer
+	status     string
+	groupID    string
 	breaker    *breaker.Breaker
-	reConnect  chan bool
 	statusLock sync.Mutex
 	exit       bool
 }
 
-//KafkaMessageHandler  消费者回调函数
+// KafkaMessageHandler  消费者回调函数
 type KafkaMessageHandler func(message *sarama.ConsumerMessage) (bool, error)
 
-//kafka 消费者配置
+// kafka 消费者配置
 func getKafkaDefaultConsumerConfig() (config *cluster.Config) {
 	config = cluster.NewConfig()
 	config.Consumer.Return.Errors = true
@@ -59,7 +58,7 @@ func getKafkaDefaultConsumerConfig() (config *cluster.Config) {
 	return
 }
 
-//启动消费者
+// 启动消费者
 func StartKafkaConsumer(hosts, topics []string, groupID string, config *cluster.Config, f KafkaMessageHandler) (*Consumer, error) {
 	var err error
 	if config == nil {
@@ -71,10 +70,8 @@ func StartKafkaConsumer(hosts, topics []string, groupID string, config *cluster.
 		status:  KafkaConsumerDisconnected,
 		groupID: groupID,
 		topics:  topics,
-
-		breaker:   breaker.New(3, 1, 3*time.Second),
-		reConnect: make(chan bool),
-		exit:      false,
+		breaker: breaker.New(3, 1, 3*time.Second),
+		exit:    false,
 	}
 
 	if consumer.consumer, err = cluster.NewConsumer(hosts, groupID, topics, consumer.config); err != nil {
@@ -83,7 +80,6 @@ func StartKafkaConsumer(hosts, topics []string, groupID string, config *cluster.
 		consumer.status = KafkaConsumerConnected
 		logger.Info("kafka consumer started", zap.Any(groupID, topics))
 	}
-	go consumer.keepConnect()
 	go consumer.consumerMessage(f)
 
 	return consumer, err
@@ -98,52 +94,7 @@ func (c *Consumer) Close() error {
 
 }
 
-//检查kafka连接状态,如果断开链接则尝试重连
-func (c *Consumer) keepConnect() {
-	for !c.exit {
-		select {
-		case <-c.reConnect:
-			if c.status != KafkaConsumerDisconnected {
-				break
-			}
-
-			logger.Warn("KafkaConsumer reconnecting", zap.Any(c.groupID, c.topics))
-			var consumer *cluster.Consumer
-		breakLoop:
-			for {
-				//利用熔断器给集群以恢复时间，避免不断的发送重联
-				err := c.breaker.Run(func() (err error) {
-					consumer, err = cluster.NewConsumer(c.hosts, c.groupID, c.topics, c.config)
-					return
-				})
-
-				switch err {
-				case nil:
-					c.statusLock.Lock()
-					if c.status == KafkaConsumerDisconnected {
-						c.consumer = consumer
-						c.status = KafkaConsumerConnected
-					}
-					c.statusLock.Unlock()
-					break breakLoop
-				case breaker.ErrBreakerOpen:
-					logger.Warn("kafka  consumer connect fail, broker is open")
-					//5 s之后，促发重新重连接，此时熔断器刚好 half close
-					if c.status == KafkaConsumerDisconnected {
-						time.AfterFunc(5*time.Second, func() {
-							c.reConnect <- true
-						})
-					}
-					break breakLoop
-				default:
-					logger.Error("kafka  consumer connect error", zap.Error(err))
-				}
-			}
-		}
-	}
-}
-
-//消费消息
+// 消费消息
 func (c *Consumer) consumerMessage(f KafkaMessageHandler) {
 	//在消费者未和 kafka 建立连接时，不消费 kafka 数据
 	for !c.exit {
@@ -181,17 +132,16 @@ func (c *Consumer) consumerMessage(f KafkaMessageHandler) {
 				}
 			case err := <-c.consumer.Errors():
 				logger.Error("kafka consumer msg error ", zap.Error(err))
-				//需要捕获 kafka 中断信息，尝试重新连接 kafka
+				//需要捕获 kafka 中断信息
 				if errors.Is(err, sarama.ErrOutOfBrokers) || errors.Is(err, sarama.ErrNotConnected) {
 					c.statusLock.Lock()
 					if c.status == KafkaConsumerConnected {
 						c.status = KafkaConsumerDisconnected
-						c.reConnect <- true
 					}
 					c.statusLock.Unlock()
 				} else {
 					// 如果不是中断信息,认为kafka挂了,进程退出
-					// panic("kafka server error:" + err.Error())
+					logger.Error("kafka server error:", zap.Error(err))
 				}
 			case s := <-signals:
 				// 收到系统消息先打印
